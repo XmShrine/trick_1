bb51a362b3485001786b864dd7596818a2b2189c
XmShrine <xmshrine@163.com>
2025-08-07 21:04:19 +0800

    v19

--- playAudio.js
+++ playAudio.js
@@ -37,28 +37,6 @@ async function playAudio(src) {
 
     loadAudio(src);
 
-    // // 确保 AudioContext 存在
-    // if (!audioCtx) {
-    //     audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-    // }
-
-    // // 检查是否已经加载过该音频文件
-    // if (!audioBuffers.has(src)) {
-    //     try {
-    //         // 从服务器加载音频文件
-    //         const response = await fetch(src);
-    //         const arrayBuffer = await response.arrayBuffer();
-    //         const audioBuffer = await audioCtx.decodeAudioData(arrayBuffer);
-            
-    //         // 缓存解码后的音频数据
-    //         audioBuffers.set(src, audioBuffer);
-    //         console.log(`音频文件 ${src} 已加载并解码。`);
-    //     } catch (error) {
-    //         console.error(`音频文件 ${src} 加载或解码失败:`, error);
-    //         throw error; // 抛出错误，让调用者可以捕获
-    //     }
-    // }
-
     // 获取缓存的音频数据
     const audioBuffer = audioBuffers.get(src);
 
