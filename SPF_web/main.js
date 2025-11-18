95056f15cef044c84a56e0a624637f2fb824ebe1
XmShrine <xmshrine@163.com>
2025-11-18 14:43:17 +0800

    save

--- SPF_web/main.js
+++ SPF_web/main.js
@@ -319,7 +319,7 @@ function SPFHelper(pol, helper) {
     }
     for (var i=0; i<len; i++) {
         res[i] = (i!=len-1)?mon[i]-mon[i+1]:mon[i];
-        tmp = Polynomial.power(helper.get(i), res[i]);
+        var tmp = Polynomial.power(helper.get(i), res[i]);
         temp = Polynomial.mult(temp, tmp);
     }
     temp = Polynomial.mult(Polynomial.number(-pol.arr[0].coeff), temp);
